44cc406edab32f67c5f74e276e71436ed708e921
Mickey Lax <mickeylax_07@yahoo.com>
2026-05-26 16:38:10 -0400

    Began and worked through Q5 on SQL_basics

--- Week1/AssignmentFiles/SQL_basics.sql
+++ Week1/AssignmentFiles/SQL_basics.sql
@@ -7,13 +7,22 @@ USE coffeeshop_db;
 -- =========================================================
 
 -- Q1) List all products (show product name and price), sorted by price descending.
-
+select name, price 
+from products
+order by price desc;
 -- Q2) Show all customers who live in the city of 'Lihue'.
-
--- Q3) Return the first 5 orders by earliest order_datetime (order_id, order_datetime).
-
+select customer_id, first_name, last_name 
+from customers
+where city = "Lihue";
+-- Q3) Return the first 5 orders by earliest order_datetime (order_id, order_datetime)
+select order_id, order_datetime 
+from orders
+order by order_datetime asc
+limit 5;
 -- Q4) Find all products with the word 'Latte' in the name.
-
+select *
+from products 
+where name like "%Latte%";
 -- Q5) Show distinct payment methods used in the dataset.
 
 -- Q6) For each store, list its name and city/state (one row per store).
